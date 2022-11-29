eb4c983f5ea96d7f20f8463e114ae4b95da0aeee
Connor Bray <connor.bray@maine.edu>
2022-11-29 09:42:44 -0500

    s

--- controller.c
+++ controller.c
@@ -13,16 +13,13 @@ int main(int argc, char **argv) {
 
 		temp = read_temp();
 
-		// check if temp is outlier??
-
 		fd = init_display();
 
 		write_display(fd, temp);
 
 		usleep(1000000);
+	
 	}
-
-	// ?? When will it break out of loop? should it do this every time it is written?
 	shutdown_display(fd);
 
 	return 0;
