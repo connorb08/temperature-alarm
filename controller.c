6662e7330c5b2d68fd22e6a82bfb4d4a4f961d7d
Connor Bray <connor.bray@maine.edu>
2022-12-06 10:29:17 -0500

    commit

--- controller.c
+++ controller.c
@@ -22,52 +22,6 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	// if (argc > 1) {
-
-	// 	if (strcmp(argv[1], "r") == 0) {
-	// 		printf("%lf", read_temp());
-	// 		//return read_temp();
-	// 	}
-
-	// 	if (strcmp(argv[1], "i") == 0) {
-	// 		printf("%d", init_display());
-	// 		return 0;
-	// 		//return (int) init_display();
-	// 	}
-
-	// 	if (strcmp(argv[1], "w") == 0) {
-	// 		if (argc > 3) {
-	// 			fd = (int) atoi(argv[2]);
-	// 			temp = (double) atof(argv[3]);
-	// 			printf("fd: %d, temp: %f", fd, temp);
-	// 			write_display(fd, temp);
-	// 			// turn arg[2] into fd
-	// 			// turn arg[3] into temp
-	// 		}
-	// 		else {
-	// 			return -1;
-	// 		}
-	// 	}
-
-	// 	if (strcmp(argv[1], "c") == 0) {
-	// 		if (argc > 2) {
-	// 			fd = atoi(argv[2]);
-	// 			shutdown_display(fd);
-	// 			//shutdown_display(fd);
-	// 			// turn arg2 into fd
-	// 		}
-			
-	// 	}
-
-	// }
-	// else {
-	// 	fd = init_display();
-	// 	temp = read_temp();
-	// 	write_display(fd, temp);
-	// 	printf("ex");
-	// 	return -1;
-	// }
-
 	return 0;
 }
 
--- controller.py
+++ controller.py
@@ -2,6 +2,8 @@ import json
 import subprocess
 import time
 import datetime
+import smtplib
+import sys
 
 class Controller():
     
@@ -19,24 +21,22 @@ class Controller():
         self.lastAlert = datetime.datetime(2000, 1, 1, 1, 1, 1)
         self.emails = config['emails']
         self.phones = config['phone_numbers']
-
-        print(self.emails, self.phones)
+        self.auth = (config['auth']['email'],config['auth']['password'])
+        self.message = config['alert_message']
     
     def shutdown(self):
         args = ['c']
-        return self.exec(args)
+        return self.getTemp(args)
 
-    def alert(self):
+    def alert(self, temp):
         now = datetime.datetime.now()
         difference = now - self.lastAlert
         mins = difference.total_seconds() / 60
         
         if (mins > self.alert_interval):
-            # alert()
             print("alert")
+            self.sendNotifications(temp)
             self.playAudio()
-            #send notificaiton
-            #sound alarm
             self.lastAlert = datetime.datetime.now()
         
         return
@@ -44,18 +44,32 @@ class Controller():
     def playAudio(self):
         subprocess.run(['amixer', 'sset', 'Master', f'{self.volume}%'])
         subprocess.run(['aplay', 'alert.wav'])
+        sys.stdout.flush()
         return
 
-    def sendNotifications(self):
+    def sendNotifications(self, temp):
+        
         for email in self.emails:
-            #send email
-            pass
-
+            self.sendEmail(email, temp)
         for number in self.phones:
-            #send text
-            pass
-
+            self.sendText(number, temp)
         return
+    
+    def sendEmail(self, email, temp):
+        msg = self.message + str(temp) + "F"
+        to = email
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self.auth[0], self.auth[1])
+        server.sendmail(self.auth[0], to, msg)
+
+    def sendText(self, number, temp):
+        msg = self.message + str(temp) + "F"
+        to = str(number) + "@vtext.com"
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self.auth[0], self.auth[1])
+        server.sendmail(self.auth[0], to, msg)
 
     def getTemp(self, args=[]):
         args.insert(0, './controller')
@@ -73,7 +87,6 @@ class Controller():
     def tester(self, temp=0):
 
         args = ['./test']
-
         if temp != 0:
             temp = str(temp)
             args.append(temp)
@@ -81,25 +94,33 @@ class Controller():
         popen = subprocess.Popen(args, stdout=subprocess.PIPE)
         popen.wait()
         out = popen.stdout.read()
-        return float(out)
+        out = round(float(str(out)[2:-1]), 2)
+        return out
 
 def main():
     x = Controller()
     temps = []
     while (True):
-        #catch q or ctrl c
-        if (len(temps) > 0):
-            temp = x.tester(temps[-1])
-        else:
-            temp = x.tester()
-        temps.append(temp)
-        avg = (sum(temps) / len(temps))
-        print(f"avg: {avg}")
-        if (len(temps) >= 10):
-            temps.pop(0)
-            if ((avg < x.temp_low) or (avg > x.temp_high)):
-                x.alert()
-        time.sleep(1)
+        try:
+            if (len(temps) > 0):
+                temp = x.tester(temps[-1])
+            else:
+                temp = x.tester()
+            print(temp)
+            temps.append(temp)
+            avg = (sum(temps) / len(temps))
+            print(f"avg: {avg}")
+            s = x.message + str(temp) + "F"
+            print(s)
+            if (len(temps) >= 10):
+                temps.pop(0)
+                if ((avg < x.temp_low) or (avg > x.temp_high)):
+                    x.alert(temp)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("\nShutting down display and exiting!")
+            x.shutdown()
+            exit(1)
 
 if __name__ == '__main__':
     main()
